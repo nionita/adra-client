d8c22dc415166ec86ea93f5e1e992a2907eada07
Nicu Ionita <nicu@ionita.at>
2019-05-30 19:22:35 +0200

    Access both ID and block APIs, reorganize code

---
+++ src/API.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { read, write } from './operations'
+  export let api
+	let message = ''
+	let value = ''
+
+	async function doWrite() {
+    message = await write(api, value)
+	}
+
+	async function doRead() {
+    const rr = await read(api)
+    message = rr.message
+    value = rr.value
+  }
+</script>
+
+<div class="api">
+	<div class="title">
+		<h2>{api} API</h2>
+	</div>
+	<div class="text">
+		<div class="group">
+			<button on:click="{doRead}">Read</button>
+			<input type="text" id="block" bind:value="{value}">
+			<button on:click="{doWrite}">Write</button>
+		</div>
+		<div class="message">
+			<input type="text" id="message" disabled="true" value="{message}">
+		</div>
+	</div>
+</div>
+
+<style>
+	h2 {
+		color: purple;
+	}
+
+	.api {
+    margin: 2px;
+    padding: 2px;
+    border: 1px solid rgb(200, 200, 200);
+	}
+
+	.group {
+		display: flex;
+	}
+
+	.text, .title {
+		width: 100%;
+		/* margin: auto; */
+	}
+
+	#block {
+		width: 80%;
+	}
+
+	#message {
+		width: 100%;
+	}
+</style>
---
+++ src/operations.js
@@ -0,0 +1,62 @@
+const url_base = 'http://localhost:7200/api/'
+
+const write = (api, value) => {
+  console.log('Want to write', value)
+  const body = api === 'id' ? { id: Number(value) } : { block: value }
+  const myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/json')
+  const myInit = {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify(body)
+  }
+  const url = url_base + api
+  const writeRequest = new Request(url)
+  return fetch(writeRequest, myInit)
+    .then(response => {
+      console.log(response)
+      if (!response.ok)
+        throw new Error('HTTP error, status = ' + response.status)
+      return response.json()
+    })
+    .then(json => {
+      console.log(json)
+      return JSON.stringify(json)
+    })
+    .catch(e => {
+      console.log('Exception in fetch:', e)
+      return toString(e)
+    })
+}
+
+const read = api => {
+  let value = ''
+  const myInit = {
+    method: 'GET',
+  }
+  const url = url_base + api
+  const readRequest = new Request(url)
+  return fetch(readRequest, myInit)
+    .then(response => {
+      console.log(response)
+      if (!response.ok) {
+        throw new Error('HTTP error, status = ' + response.status)
+      }
+      return response.json()
+    })
+    .then(json => {
+      console.log(json)
+      const message = JSON.stringify(json)
+      if (api in json) {
+        value = json[api]
+      }
+      return { message, value }
+    })
+    .catch(e => {
+      console.log('Exception in fetch:', e)
+      const message = toString(e)
+      return { message, value }
+    })
+}
+
+export { read, write }
